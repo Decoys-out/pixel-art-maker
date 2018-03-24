66423479c1a51d247ca5b5acb775c354947d34ba
Gaganjot Singh <32265569+Decoys-out@users.noreply.github.com>
2018-03-24 12:39:03 +0530

    Added comments and other changes

--- project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/project-pixel-art-maker-starter-master/designs.js
@@ -1,12 +1,14 @@
 $(function () {
+    //defining the variables
     let height = $('#inputHeight');
     let width = $('#inputWeight');
     let canvas = $('#pixelCanvas');
     let pickColor = $('#colorPicker');
-
+//function to create the grid
     function makeGrid() {
         let noOfRows = height.val();
         let noOfCols = width.val();
+        canvas.find('tbody').remove()
         canvas.append('<tbody></tbody>')
         let table = canvas.find('tbody');
         for (let i = 0; i < noOfRows; i++) {
@@ -16,6 +18,7 @@ $(function () {
             canvas.find('tr').append('<td class="background"></td>');
         }
     }
+    //function to cal the make grid function
     $(document).ready(function () {
         $('#submit').on('click', function (e) {
             e.preventDefault();
@@ -26,7 +29,7 @@ $(function () {
             let color = pickColor.val();
             let apply = $(this).css('background-color');
             console.log(apply + " color:" + color);
-
+            //to apply the color in the cell
             if ($(this).hasClass('background')) {
                 $(this).toggleClass('background');
                 $(this).css("background-color", color);
